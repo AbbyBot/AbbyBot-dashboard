74945bb40b0e566b4d45b3e35ba14f4b81d8c0a2
Fernando Catalan <afk656902@hotmail.com>
2024-09-13 06:05:07 -0300

    Add SCSS Support
    Note: if you have cloned this before, please run: "npm install" again. I added some packages
    
    Commit notes:
    Added sass support to manage Dashboard themes through SCSS. Abby Theme was created too with a template for copy-paste :D

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ function App() {
 
   return <>
     <h1>Hello world!</h1>
+    <div className="bg" style={{width: 30, height:30}}></div>
+    <div className="bg-primary" style={{width: 30, height:30}}></div>
+    <div className="bg-secondary" style={{width: 30, height:30}}></div>
+    <div className="bg-tertiary" style={{width: 30, height:30}}></div>
+    <div className="bg-warning" style={{width: 30, height:30}}></div>
+    <button className="btn-secondary">
+      I'm a button
+    </button>
   </>
 }
 
