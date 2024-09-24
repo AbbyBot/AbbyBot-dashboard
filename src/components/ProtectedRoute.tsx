510d907a0c8d498f589cd331e1fc5caf8d38acfa
Fernando Catalan <afk656902@hotmail.com>
2024-09-23 23:29:35 -0300

    minor change

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { useContext, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { getCookie } from "typescript-cookie";
 
 export default function ProtectedRoute() {
   const {isAuthenticated, login, loading} = useContext(AuthContext);
   useEffect(() => {
-    if (!loading) {
+    if (!loading || !getCookie("at")) {
       if(!isAuthenticated) {
         login();
         return;
