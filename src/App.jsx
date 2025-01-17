797361a753c01fb4ab4e6cf75d3ec2b60273df37
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-17 20:53:04 -0300

    refactor: remove loading simulation and unused Init component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import { LoadingProvider, useLoading } from './context/LoadingContext';
-import Init from './components/Init';
+import { LoadingProvider } from './context/LoadingContext';
 
 function App() {
-  const { setIsLoading } = useLoading();
-
-  useEffect(() => {
-    // Simulate loading of other components
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000); // Adjust this timeout as needed
-
-    return () => clearTimeout(timer);
-  }, [setIsLoading]);
-
+  
   return (
     <RouterProvider router={router} />
   );
@@ -24,7 +13,6 @@ function App() {
 export default function Root() {
   return (
     <LoadingProvider>
-      <Init />
       <App />
     </LoadingProvider>
   );
