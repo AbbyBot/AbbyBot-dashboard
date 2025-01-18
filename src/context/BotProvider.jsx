cf2fa7152e69748e44cba012c08a246443719cba
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-17 23:18:12 -0300

    refactor: update BotProvider to use null for initial status and serversCount, and remove loading state

--- src/context/BotProvider.jsx
+++ src/context/BotProvider.jsx
@@ -1,34 +1,34 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { ABBYBOT_API_URL } from "../environ";
 import axios from "axios";
 import capitalize from "capitalize";
 export const BotContext = createContext({
     getStatus: async () => { },
     status: 'offline',
-    isLoading: false,
-    serversCount: 0
+    serversCount: null
 })
 
 export const BotProvider = ({ children }) => {
-    const [status, setStatus] = useState('Offline')
-    const [serversCount, setCount] = useState(0)
-    const [isLoading, setLoading] = useState(false)
+    const [status, setStatus] = useState(null);
+    const [serversCount, setCount] = useState(null);
 
     const getStatus = async () => {
         try {
-            setLoading(true)
             let botInfo = await axios.get(`${ABBYBOT_API_URL}/bot-info`);
             setStatus(capitalize(botInfo.data.status));
-            setCount(botInfo.data.server_count)
+            setCount(botInfo.data.server_count);
         } catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
+            console.log(error);
         }
+    };
 
-    }
+    useEffect(() => {
+        getStatus();
+    }, []);
 
-    return <BotContext.Provider value={{ getStatus, status, isLoading, serversCount }}>
-        {children}
-    </BotContext.Provider>
-}
+    return (
+        <BotContext.Provider value={{ getStatus, status, serversCount }}>
+            {children}
+        </BotContext.Provider>
+    );
+};
