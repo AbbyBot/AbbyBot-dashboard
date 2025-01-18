d4f162e8e924b4f03a4e9a4e071883769293fd4f
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-17 21:23:22 -0300

    fix: update logo filename and adjust image styles in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,13 @@ import React, { useContext, useEffect } from 'react'
 import { Dropdown } from './Dropdown'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGlobeAmericas, faSignIn } from '@fortawesome/free-solid-svg-icons'
-import abbylogo from '../assets/abbybot-logo.png'
+import abbylogo from '../assets/abbybot_logo.png'
 import { AuthContext } from '../context/AuthContext'
 import { DISCORD_AVATAR_URL } from '../environ'
 import { useNavigate, Link } from 'react-router-dom'
 import { useModal } from '../context/ModalProvider'
 import { Modal } from './Modal'
+import { width } from '@fortawesome/free-brands-svg-icons/fa42Group'
 
 export default function Header() {
     const { user, logout, login } = useContext(AuthContext);
@@ -33,7 +34,7 @@ export default function Header() {
             <nav className='navbar'>
                 <div className='d-flex'>
                     <Link to='/' className='brand'>
-                        <img src={abbylogo} alt='Abbybot Logo' width={30} height={30} />
+                        <img src={abbylogo} alt='Abbybot Logo' style={{ width: 40, height: 40, borderRadius: '50%' }} />
                         <span className='text-light'>AbbyBot Dashboard</span>
                     </Link>
                     <Dropdown>
@@ -42,7 +43,7 @@ export default function Header() {
                             AbbyBotProject
                         </Dropdown.Button>
                         <Dropdown.Body id='abbybot-project-dropdown'>
-                            <Dropdown.Item onClick={() => window.open("https://www.abbybotproject.com", "_blank")}>AbbyBot Project (website)</Dropdown.Item>
+                            <Dropdown.Item onClick={() => window.open("https://abbybotproject.com", "_blank")}>AbbyBot Project (website)</Dropdown.Item>
                             <Dropdown.Item onClick={() => window.open("https://github.com/abbybot", "_blank")}>GitHub Organization</Dropdown.Item>
                         </Dropdown.Body>
                     </Dropdown>
@@ -51,7 +52,7 @@ export default function Header() {
                     {user ?
                         <Dropdown>
                             <Dropdown.Button bodyId='abbybot-user-dropdown'>
-                                <img src={DISCORD_AVATAR_URL + `${user.data.id}/${user.data.avatar}?size=16`} className='rounded' alt="" />
+                                <img src={DISCORD_AVATAR_URL + `${user.data.id}/${user.data.avatar}?size=256`} style={{ width: 40, height: 40, borderRadius: '50%' }} alt="Discord user image" />
                                 {user.data.username}
                             </Dropdown.Button>
                             <Dropdown.Body id='abbybot-user-dropdown'>
